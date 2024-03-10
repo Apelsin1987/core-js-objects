fcc1b26f5ed7871c2c0880bfbc329c2a595475f1
Konstantin Si <apelsin1987@gmail.com>
2024-03-10 14:29:31 +0100

    feat: solve fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -213,8 +213,14 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const param = JSON.parse(json);
+  const obj = Object.create(proto);
+
+  Object.entries(param).forEach(([key, value]) => {
+    obj[key] = value;
+  });
+  return obj;
 }
 
 /**
